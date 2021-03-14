15a4b19534db6bb42271246f0de2f71af957ad4f
Mateusz Gibuła <matgibula@gmail.com>
2021-03-14 07:54:44 +0100

    init package

--- tube/src/manager/index.ts
+++ tube/src/manager/index.ts
@@ -1,4 +1,14 @@
-export default class Services {
-  constructor() {}
+import amqp, { Connection } from "amqplib";
+import Bluebird from "bluebird";
+export default class Broker {
+  private url: string;
+  private connection: Connection | null;
+  constructor(url: string) {
+    this.url = url;
+    this.connection = null;
+  }
+  async init() {
+    this.connection = await amqp.connect(this.url);
+  }
   get(name: string) {}
 }
