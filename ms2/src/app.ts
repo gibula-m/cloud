694140a0cc28697bb412de6dc42d9d38efc5809e
Mateusz Gibuła <matgibula@gmail.com>
2021-03-17 18:09:52 +0100

    revert

--- ms2/src/app.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-import { logger, LogTypes } from "./util/logger";
-import map from "./services";
-dotenv.config();
-logger(LogTypes.INFO, process.env.APP_NAME!);
--- ms2/src/services/hello2.ts
+++
@@ -1,3 +0,0 @@
-export const hello2 = () => {
-  console.log("HELLO2 SERVICE");
-};
